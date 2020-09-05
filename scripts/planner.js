42cda95b60bc0b979e688947c969946b6d6ab5aa
troylochner <troy.lochner@gmail.com>
2020-09-05 14:39:42 -0400

    finally have a look I like

--- scripts/planner.js
+++ scripts/planner.js
@@ -15,43 +15,41 @@ theDate = getDate() ;
 $("#currentDay").append(theDate);
 
 //FOR EACH HOUR IN THE DAY - MAKE A ROW
-for ( i=0; i < 24 ; i++ ){
+//ADJUSTED TIME - DOING 7 TO 7
+for ( i=7; i < 20 ; i++ ){
 var displayTime = moment().hour(i).minute(0).second(0) //THIS CAN ACT AS A GOOD UUID-TYPE INDEX - THIS IS OUR EPOCH DATE
 var nextTime = moment().hour(i+1).minute(0).second(0)
 
 //console.log(moment(calTime).utc());
 
 //MAKE TABLE ROW ELEMENT
-var blockRow = $('<tr>')
-blockRow.attr("data-id",displayTime);
+var row = $("<div>").addClass("row").attr("data-id",displayTime).css("width", "100%");
+
+
+//MAKE THE TABLE DATA ELEMENT - DISPLAY TIME OF DAY
+var blockTime = $("<div>").addClass("col-md-2 hour").text(moment(displayTime).format("LT"));
+
+//MAKE TABLE DATA - DISPLAY WHAT IS HAPPENING AT THAT TIME
+var blockContent = $("<input>").addClass("col-md-8 description");
+blockContent.attr("placeholder","Enter an event here...")
 
 //CONDITIONAL IF THE TIME IS IN THE PAST / PRESENT / FUTURE
 if (moment().isBetween(displayTime,nextTime)===true){
-    blockRow.addClass("present")
+    blockContent.addClass("present")
 } else if (moment().isBefore(displayTime)){
-    blockRow.addClass ("past")
+    blockContent.addClass ("future")
 } else {
-    blockRow.addClass("future")
+    blockContent.addClass("past")
 }
 
-//MAKE THE TABLE DATA ELEMENT - DISPLAY TIME OF DAY
-var blockTime = $('<td>'+ moment(displayTime).format('LT') + '</td>');
-//blockHeader.addClass("time-block")
-
-//MAKE TABLE DATA - DISPLAY WHAT IS HAPPENING AT THAT TIME
-var blockContent = $('<td><textarea>'+'something'+'</textarea></td>');
-//blockContent.addClass("time-block description")
-
 //MAKE BUTTON FOR THE ROW
-var blockButton=$('<button>');
-//blockButton.attr("data-id"+i)
-//blockButton.addClass("saveBtn");
+var saveButton = $("<button>").addClass("col-md-2 saveBtn").attr("data-id", displayTime).text("Save");
+
 
 //PUT IT ALL TOGETHER
-blockRow.append(blockTime,blockContent,blockButton);
-//MAKE STYLE FOR TIME   $("h1, h2, p").addClass("blue");
-//SET INDEX FOR EACH NOW ROW MADE   $("#w3s").attr("href", "https://www.w3schools.com/jquery/");
-$('#timeline').append(blockRow);
+row.append(blockTime).append(blockContent).append(saveButton);
+
+$('#calendar').append(row);
 
 };}
 
