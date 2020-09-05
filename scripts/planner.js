3c6fedeb5524b690e9d4b8948b5d6d2bb5fbed21
troylochner <troy.lochner@gmail.com>
2020-09-05 18:39:27 -0400

    styling

--- scripts/planner.js
+++ scripts/planner.js
@@ -22,11 +22,8 @@ function initCal() {
         var displayTime = moment().hour(i).minute(0).second(0) //THIS CAN ACT AS A GOOD UUID-TYPE INDEX - THIS IS OUR EPOCH DATE
         var nextTime = moment().hour(i + 1).minute(0).second(0)
 
-        //console.log(moment(calTime).utc());
-
         //MAKE TABLE ROW ELEMENT
         var row = $("<div>").addClass("row").attr("data-id",i).css("width", "100%");
-
 
         //MAKE THE TABLE DATA ELEMENT - DISPLAY TIME OF DAY
         var blockTime = $("<div>").attr("data-id",i).addClass("col-md-2 hour").text(moment(displayTime).format("LT"));
@@ -48,16 +45,11 @@ function initCal() {
         var foundEvent = findEvent(i);
         
         if (foundEvent !== null){
-           //console.log("initCal -> foundEvent", foundEvent.Title)
            var title = foundEvent.Title
-           console.log("initCal -> title", title)
            blockContent.val(title);
         } 
-
         //MAKE BUTTON FOR THE ROW
         var saveButton = $("<button>").addClass("col-md-2 saveBtn").attr("data-id",i).text("Save");
-
-
         //PUT THE ROW TOGETHER AND APPEND TO THE CALENDAR ELEMENT
         row.append(blockTime).append(blockContent).append(saveButton);
         $('#calendar').append(row);
@@ -70,26 +62,18 @@ function findEvent(id) {
     return x;
  }
 
-
+$(document).ready(function(){
 //SAVE DATA
 $(".saveBtn").on("click", function () {
     eventTitle = $(this).siblings(".description").val().trim();
-    console.log("eventTitle", eventTitle)
     eventID = $(this).siblings(".description").attr("id");
-    console.log("eventID", eventID)
     newEvent = { 'eventID' : eventID, 'Title' : eventTitle };
     newEvent = JSON.stringify(newEvent);
     localStorage.setItem("storedEvents[" +eventID+']', newEvent );
-
-    var ds_items = JSON.parse(localStorage.getItem("test"));
-    console.log("ds_items", ds_items);
 })
 
 $("#clearAll").on("click", function () {
     localStorage.clear();
     initCal();
 })
-
-
-$(document).ready(function(){
 });
